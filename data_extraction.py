971fd7e995d05629c0e293ac256d363cb1efa596
minhphan6624 <quangminh6624@gmail.com>
2024-10-02 14:07:21 +1000

    Extracted data

--- data_extraction.py
+++ data_extraction.py
@@ -1,14 +1,14 @@
 import os
 import pandas as pd
 
-input_folder = ""
-output_folder = ""
+input_folder = "data"
+velocity_output_folder = "extracted_data/velocity"
+acceleration_output_folder = "extracted_data/acceleration"
 
-# Make sure the folders exists
-if not os.path.exists(input_folder):
-    os.makedirs(input_folder)
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+# Make sure output folders exist
+os.makedirs(input_folder, exist_ok=True)
+os.makedirs(velocity_output_folder, exist_ok=True)
+os.makedirs(acceleration_output_folder, exist_ok=True)
 
 # Loop through each excel file
 for file_name in os.listdir(input_folder):
@@ -21,7 +21,15 @@ for file_name in os.listdir(input_folder):
         velocity_df = xls.parse('Segment Velocity')
         acceleration_df = xls.parse('Segment Acceleration')
 
-        # Save each new dataframe to a new csv file
+        # Create output paths
+        velocity_output_path = os.path.join(
+            velocity_output_folder, f"{file_name.split('.')[0]}_Velocity.csv")
 
-        # Save the extracted data to a new excel file
-        df.to_excel(os.path.join(output_folder, file_name), index=False)
+        acceleration_output_path = os.path.join(
+            acceleration_output_folder, f"{file_name.split('.')[0]}_Acceleration.csv")
+
+        # Save the extracted data to a new csv file
+        velocity_df.to_csv(velocity_output_path, index=False)
+        acceleration_df.to_csv(acceleration_output_path, index=False)
+
+        print(f"Processed and saved: {file_name}")
